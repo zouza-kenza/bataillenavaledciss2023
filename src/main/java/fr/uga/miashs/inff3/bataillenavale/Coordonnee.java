8905adb1d4ca1ca6ed47c7c44f23c5d4b3508a70
zouza-kenza <120374566+zouza-kenza@users.noreply.github.com>
2023-12-11 16:43:52 +0100

    Update Coordonnee.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/Coordonnee.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/Coordonnee.java
@@ -1,45 +1,73 @@
 package fr.uga.miashs.inff3.bataillenavale;
 
 public class Coordonnee {
+private int ligne;
+	private int colonne;
 	
+	public Coordonnee(int ligne, int colonne) {
+		if (ligne < 0 || ligne > 25 || colonne < 0 || colonne > 25) {
+			throw new IllegalArgumentException("Les coordonnées de ligne et de colonne sont éronnées.");
+		}
+		this.ligne = ligne;
+		this.colonne = colonne;
+	}
 	public Coordonnee(String s) {
-		// A modifier
+		if (s.length() < 2 || s.length() > 3) {
+			throw new IllegalArgumentException(
+					"La coordonnee depasse les limites elle doit prendre 2 ou 3 caracteres)");
+		}
+		this.colonne = s.charAt(0) - 'A';
+		this.ligne = Integer.parseInt(s.substring(1)) - 1;
+		
 	}
-	
-	public Coordonnee(int l, int c) {
-		// A modifier
+	public String toString() {
+		String coordonnees = new String();
+		return coordonnees + (char) (this.colonne + 'A') + (this.ligne + 1);
+		
 	}
-	
-	public int getLigne() {
-		// A modifier
-		return 0;
-	}
-	
 	public int getColonne() {
-		// A modifier
-		return 0;
+		return this.colonne;
+		
 	}
-	
-	public boolean equals(Object o) {
-		if (o instanceof Coordonnee) {
-			Coordonnee c = (Coordonnee) o;
-			// A modifier
+	public int getLigne() {
+		return this.ligne;
+		
+	}
+
+	/*
+	 * public boolean equals(Object obj) { return (obj instanceof Coordonnee) &&
+	 * (compareTo((Coordonnee) obj) == 0); }
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof Coordonnee) {
+			Coordonnee c = (Coordonnee) obj;
+			return (this.ligne == c.ligne && this.colonne == c.colonne);
 		}
 		return false;
 	}
-	
 	public boolean voisine(Coordonnee c) {
-		// A modifier
-		return true;
+		if (this.colonne == c.colonne) {
+			if (this.ligne == c.ligne + 1 || this.ligne == c.ligne - 1)
+				return true;
+		}
+		if (this.ligne == c.ligne) {
+			if (this.colonne == c.colonne + 1 || this.colonne == c.colonne - 1)
+				return true;
+		}
+		return false;
+		
 	}
-	
+	@Override
 	public int compareTo(Coordonnee c) {
-		// A modifier
-		return 0;
+			if ((this.ligne == c.ligne && this.colonne < c.colonne) || ((this.ligne < c.ligne) && (this.colonne==c.colonne)))
+				return -1;
+			else if (this.ligne == c.ligne && this.colonne == c.colonne)
+				return 0;
+			else
+				return 1;
+		
 	}
 	
-	public String toString() {
-		// A modifier
-	   return super.toString();
-	}
+	
+
 }
