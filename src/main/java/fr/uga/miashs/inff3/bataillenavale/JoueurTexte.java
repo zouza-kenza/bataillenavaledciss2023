9bd0ad0ee9da26d68a8a51a9d40409763757144b
Montana <114038044+ChrysalideFI@users.noreply.github.com>
2023-12-12 11:07:09 +0100

    Update JoueurTexte.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurTexte.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurTexte.java
@@ -1,1 +1,29 @@
+package fr.uga.miashs.inff3.bataillenavale;
+
+import java.util.Scanner;
+
+public class JoueurTexte extends JoueurAvecGrille {
+	private Scanner sc;
+	
+	public JoueurTexte(GrilleNavale g, String nom) {
+		super(GrilleNavale g, String nom);
+	}
+	public JoueurTexte(GrilleNavale g) {...}
+	
+	protected void retourAttaque(Coordonnee c, int etat) {...}
+	protected void retourDefense(Coordonnee c, int etat) {...}
+	public Coordonnee choixAttaque() {...}
+
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
 
