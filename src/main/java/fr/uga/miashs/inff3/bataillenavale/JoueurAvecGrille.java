f80d82ef61cacec2e4072025763e3fc1231cd65d
Montana <114038044+ChrysalideFI@users.noreply.github.com>
2023-12-12 11:02:45 +0100

    Update JoueurAvecGrille.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAvecGrille.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAvecGrille.java
@@ -1,1 +1,37 @@
+package fr.uga.miashs.inff3.bataillenavale;
+//!! lever des exceptions !!
+public class JoueurAvecGrille extends Joueur {
+	//attribut
+	private GrilleNavale grille;
+	
+	//constructeurs
+	public JoueurAvecGrille(GrilleNavale g, String nom) {
+		nom=super.nom; //ça marche pas pourquoi ?
+		this.grille = g;
+	}
+	public JoueurAvecGrille(GrilleNavale g) {
+		this.grille = g;
+	}
+	
+	//méthode
+	public int defendre(Coordonnee c) {
+		if (c.estTouche()) {	//ne marche pas là car méthode estTouche non défini chez moi
+			return super.TOUCHE;
+		}
+		else if (c.estCoule()) {
+			return super.COULE;
+		}
+		else if (c.perdu()) {
+			return super.GAMEOVER;
+		}
+		return super.A_L_EAU;
+	}
+	
+	
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
