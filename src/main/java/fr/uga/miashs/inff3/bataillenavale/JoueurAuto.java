203a3d934a324d555085faf59fd238efad234e75
Nam DO <75475709+namdo203@users.noreply.github.com>
2023-12-13 16:13:55 +0100

    Update retourAttaque et retourDefense

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
@@ -10,11 +10,29 @@ public class JoueurAuto extends JoueurAvecGrille {
 	}
 	
 	protected void retourAttaque(Coordonnee c, int etat) {
-		
+		String res = "";
+		if (etat == Joueur.A_L_EAU)
+			res = "C'est à l'eau !";
+		else if (etat == Joueur.COULE)
+			res = "Vous avez coulé un navire !";
+		else if (etat == Joueur.GAMEOVER)
+			res = "Vous avez perdu !";
+		else if (etat == Joueur.TOUCHE)
+			res = "Vous avez touché un navire !";
+		System.out.println("Attaque en " + c + ": " + res);
 	}
 	
 	protected void retourDefense(Coordonnee c, int etat) {
-
+		String res = "";
+		if (etat == Joueur.A_L_EAU)
+			res = "C'est à l'eau !";
+		else if (etat == Joueur.COULE)
+			res = "Le navire est coulé !";
+		else if (etat == Joueur.GAMEOVER)
+			res = "Vous avez gagné !";
+		else if (etat == Joueur.TOUCHE)
+			res = "Le navire est touché !";
+		System.out.println("Attaque en " + c + ": " + res);
 	}
 	
 	public Coordonnee choixAttaque() {
