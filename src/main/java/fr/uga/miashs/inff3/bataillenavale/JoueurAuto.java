1a9f3f95c004d52dde539274fb7ecca5f9bc217d
Nam DO <75475709+namdo203@users.noreply.github.com>
2023-12-13 15:25:46 +0100

    definie les contructeurs

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
@@ -1,1 +1,24 @@
+package fr.uga.miashs.inff3.bataillenavale;
 
+public class JoueurAuto extends JoueurAvecGrille {
+	public JoueurAuto(GrilleNavale g, String nom) {
+		super(g, nom);
+	}
+	
+	public JoueurAuto(GrilleNavale g) {
+		super(g);
+	}
+	
+	protected void retourAttaque(Coordonnee c, int etat) {
+		
+	}
+	
+	protected void retourDefense(Coordonnee c, int etat) {
+
+	}
+	
+	public Coordonnee choixAttaque() {
+		
+		return null;
+	}
+}
