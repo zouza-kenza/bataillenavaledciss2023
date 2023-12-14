30f5694042786fc22f8a66687069a25ad7b56ad1
Montana <114038044+ChrysalideFI@users.noreply.github.com>
2023-12-14 12:08:03 +0100

    Update JoueurGraphique.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurGraphique.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurGraphique.java
@@ -1,28 +1,31 @@
 package fr.uga.miashs.inff3.bataillenavale;
 
 public class JoueurGraphique extends JoueurAvecGrille {
-private GrilleGraphique grilleTirs;
-public JoueurGraphique(GrilleNavaleGraphique grilleDefense,
-GrilleGraphique grilleTirs, String nom) {...}
-public JoueurGraphique(GrilleNavaleGraphique grilleDefense,
-GrilleGraphique grilleTirs) {...}
-public Coordonnee choixAttaque() {...}
-protected void retourDefense(Coordonnee c, int etat) {...}
-protected void retourAttaque(Coordonnee c, int etat) {
-Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
-grilleTirs.colorie(c, couleur);
-switch (etat) {
-case TOUCHE:
-JOptionPane.showMessageDialog(grilleTirs, "Vous avez touché
-un navire en " + c);
-break;
-case COULE:
-JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé
-un navire en " + c);
-break;
-case GAMEOVER:
-JOptionPane.showMessageDialog(grilleTirs, "Vous avez 
-gagné!!!");
-}
-}
+
+  //Attribut
+  private GrilleGraphique grilleTirs;
+
+  //Constructeurs
+  public JoueurGraphique(GrilleNavaleGraphique grilleDefense,
+  GrilleGraphique grilleTirs, String nom) {...}
+  public JoueurGraphique(GrilleNavaleGraphique grilleDefense,
+  GrilleGraphique grilleTirs) {...}
+
+  //Méthodes
+  public Coordonnee choixAttaque() {...}
+  protected void retourDefense(Coordonnee c, int etat) {...}
+  protected void retourAttaque(Coordonnee c, int etat) {
+    Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
+    grilleTirs.colorie(c, couleur);
+    switch (etat) {
+    case TOUCHE:
+      JOptionPane.showMessageDialog(grilleTirs, "Vous avez touché un navire en " + c);
+    break;
+    case COULE:
+      JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé un navire en " + c);
+    break;
+    case GAMEOVER:
+      JOptionPane.showMessageDialog(grilleTirs, "Vous avez gagné!!!");
+    }
+  }
 }
