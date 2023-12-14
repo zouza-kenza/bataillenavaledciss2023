26719be2bfb65b8d8d2a3ded059fac628c36c877
Nam DO <75475709+namdo203@users.noreply.github.com>
2023-12-14 12:20:46 +0100

    Update JoueurAuto.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
@@ -11,7 +11,7 @@ public class JoueurAuto extends JoueurAvecGrille {
 		public JoueurAuto (GrilleNavale g){
 			super(g);
 		}
-		/*
+	
 		protected void retourAttaque(Coordonnee c, int etat) {
     			this.retourAttaque(c, etat);
 		}
@@ -20,33 +20,7 @@ public class JoueurAuto extends JoueurAvecGrille {
 		protected void retourDefense(Coordonnee c, int etat) {
     			this.retourDefense(c, etat);
 		}
-		*/
 
-		protected void retourAttaque(Coordonnee c, int etat) {
-			String res = "";
-			if (etat == Joueur.A_L_EAU)
-				res = "C'est à l'eau !";
-			else if (etat == Joueur.COULE)
-				res = "Vous avez coulé un navire !";
-			else if (etat == Joueur.GAMEOVER)
-				res = "Vous avez perdu !";
-			else if (etat == Joueur.TOUCHE)
-				res = "Vous avez touché un navire !";
-			System.out.println("Attaque en " + c + ": " + res);
-		}
-		
-		protected void retourDefense(Coordonnee c, int etat) {
-			String res = "";
-			if (etat == Joueur.A_L_EAU)
-				res = "C'est à l'eau !";
-			else if (etat == Joueur.COULE)
-				res = "Le navire est coulé !";
-			else if (etat == Joueur.GAMEOVER)
-				res = "Vous avez gagné !";
-			else if (etat == Joueur.TOUCHE)
-				res = "Le navire est touché !";
-			System.out.println("Attaque en " + c + ": " + res);
-		}
 	
 		public Coordonnee choixAttaque() {
 		/*
