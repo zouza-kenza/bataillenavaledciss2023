66c85214d689dbbc5009b2931f64ba08443edbed
weebmax <152560280+weebmax@users.noreply.github.com>
2023-12-14 18:14:56 +0100

    Update JoueurGraphique.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurGraphique.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurGraphique.java
@@ -1,11 +1,12 @@
 package fr.uga.miashs.inff3.bataillenavale;
 
 import java.awt.Color;
-
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class JoueurGraphique extends JoueurAvecGrille {
 	
+	private Scanner sc;
 	private GrilleGraphique grilleTirs;
 	
 	public JoueurGraphique(GrilleNavaleGraphique grilleDefense,
@@ -18,10 +19,31 @@ public class JoueurGraphique extends JoueurAvecGrille {
 	GrilleGraphique grilleTirs) {
 		this(grilleDefense, nom);
 	}
+	public Coordonnee choixAttaque() {
+        System.out.println("Saisissez la ligne pour l'attaque : ");
+        	String ligne = sc.nextLine();
+        System.out.println("Saisissez la colonne pour l'attaque : ");
+        	String colonne = sc.nextLine();
+
+    		String coordonneeStr = ligne + colonne; 
+
+        return new Coordonnee(coordonneeStr) ;
+    	}
+	  protected void retourDefense(Coordonnee c, int etat) {
+    Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
+		grilleTirs.colorie(c, couleur);
+		switch (etat) {
+		case TOUCHE:
+		JOptionPane.showMessageDialog(grilleTirs, "Vous avez été touché en " + c);
+		break;
+		case COULE:
+		JOptionPane.showMessageDialog(grilleTirs, "Vous avez été coulé en " + c);
+		break;
+		case GAMEOVER:
+		JOptionPane.showMessageDialog(grilleTirs, "Vous avez perdu");
 	
-	public Coordonnee choixAttaque() {...}
-	protected void retourDefense(Coordonnee c, int etat) {...}
-	protected void retourAttaque(Coordonnee c, int etat) {
+	     
+	     protected void retourAttaque(Coordonnee c, int etat) {
 		Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
 		grilleTirs.colorie(c, couleur);
 		switch (etat) {
