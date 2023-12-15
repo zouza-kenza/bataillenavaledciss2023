971289fb67eba63e0c0d3717cd750d8e78091ea8
zouza-kenza <120374566+zouza-kenza@users.noreply.github.com>
2023-12-15 14:46:38 +0100

    Update JoueurGraphique.java

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurGraphique.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurGraphique.java
@@ -2,64 +2,48 @@ package fr.uga.miashs.inff3.bataillenavale;
 
 import java.awt.Color;
 import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class JoueurGraphique extends JoueurAvecGrille {
-
-	private Scanner sc;
+	
 	private GrilleGraphique grilleTirs;
-
-	public JoueurGraphique(GrilleNavaleGraphique grilleDefense, GrilleGraphique grilleTirs, String nom) {
-		super(grilleDefense, nom);
-		this.grilleTirs = grilleTirs;
-		if (nom.isEmpty() || nom == null) {
-			throw new IllegalArgumentException("Le nom est vide ou contient un ou des espace(s)");
-		}
-	}
-
-	public JoueurGraphique(GrilleNavaleGraphique grilleDefense, GrilleGraphique grilleTirs) {
-		super(grilleDefense);
-		this.grilleTirs = grilleTirs;
-	}
+	Scanner sc = new Scanner(System.in);
+	public JoueurGraphique(GrilleNavaleGraphique grilleDefense,
+			GrilleGraphique grilleTirs, String nom) {
+				super(grilleDefense, nom);
+				this.grilleTirs = grilleTirs;
+			}
 
 	public Coordonnee choixAttaque() {
-		System.out.println("Saisissez la ligne pour l'attaque : ");
-		String ligne = sc.nextLine();
-		System.out.println("Saisissez la colonne pour l'attaque : ");
-		String colonne = sc.nextLine();
-
-		String coordonneeStr = ligne + colonne;
-
-		return new Coordonnee(coordonneeStr);
+		return grilleTirs.getCoordonneeSelectionnee();
 	}
-
 	protected void retourDefense(Coordonnee c, int etat) {
-    Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
-		grilleTirs.colorie(c, couleur);
-		switch (etat) {
-		case TOUCHE:
-		JOptionPane.showMessageDialog(grilleTirs, "Vous avez été touché en " + c);
-		break;
-		case COULE:
-		JOptionPane.showMessageDialog(grilleTirs, "Vous avez été coulé en " + c);
-		break;
-		case GAMEOVER:
-		JOptionPane.showMessageDialog(grilleTirs, "Vous avez perdu");
-		}	
-	}
-
+		  Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
+			grilleTirs.colorie(c, couleur);
+			switch (etat) {
+			case TOUCHE:
+			JOptionPane.showMessageDialog(grilleTirs, "Vous avez été touché en " + c);
+			break;
+			case COULE:
+			JOptionPane.showMessageDialog(grilleTirs, "Vous avez été coulé en " + c);
+			break;
+			case GAMEOVER:
+			JOptionPane.showMessageDialog(grilleTirs, "Vous avez perdu");
+		
+			}}
 	protected void retourAttaque(Coordonnee c, int etat) {
 		Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
 		grilleTirs.colorie(c, couleur);
 		switch (etat) {
 		case TOUCHE:
-			JOptionPane.showMessageDialog(grilleTirs, "Vous avez touché un navire en " + c);
-			break;
+		JOptionPane.showMessageDialog(grilleTirs, "Vous avez touchéun navire en " + c);
+		break;
 		case COULE:
-			JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé un navire en " + c);
-			break;
+		JOptionPane.showMessageDialog(grilleTirs, "Vous avez coulé un navire en " + c);
+		break;
 		case GAMEOVER:
-			JOptionPane.showMessageDialog(grilleTirs, "Vous avez gagné!!!");
+		JOptionPane.showMessageDialog(grilleTirs, "Vous avez gagné!!!");
 		}
 	}
 }
